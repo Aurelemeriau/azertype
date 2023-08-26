983d388ef2c2b246a19dd700694d7d5dfd5bbb79
Aurelien Emeriau <aurelien.emeriau@hotmail.fr>
2023-08-26 15:40:36 +0200

    ajout fichiers azertype

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let divZoneProposition = document.querySelector(".zonePropositon")
+console.log(divZoneProposition)
+
+let spanScore = document.querySelector(".zoneScore span")
+console.log(spanScore)
+
+let listebtnradio = document.querySelectorAll(".optionSource input")
+console.log(listebtnradio)
